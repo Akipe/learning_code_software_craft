dd6383cac67a339e76438274f06f78110da50749
Julien Milletre Akipe <code.julien@milletre.fr>
2023-08-25 22:22:34 +0200

    Ajout affichage fizz lorsque multiple trois

--- kata/FizzBuzz/src/FizzBuzz.php
+++ kata/FizzBuzz/src/FizzBuzz.php
@@ -6,6 +6,10 @@ class FizzBuzz
 {
   public function print(int $number): string
   {
+    if ($number % 3 == 0) {
+      return "fizz";
+    }
+
     return (string) $number;
   }
 }
--- kata/FizzBuzz/tests/FizzBuzzTest.php
+++ kata/FizzBuzz/tests/FizzBuzzTest.php
@@ -13,4 +13,25 @@ final class FizzBuzzTest extends TestCase
     $value = $fizzBuzz->print($number);
     $this->assertSame((string) $number, $value);
   }
+
+  public function test_should_return_fizz_when_number_is_multiple_of_three(): void
+  {
+    $fizzBuzz = new FizzBuzz();
+
+    $multiplesThree = [
+      3,
+      6,
+      9,
+      12,
+    ];
+    $resultExpected = \array_fill(0, 4, "fizz");
+
+    $resultMultiplesThree = [];
+
+    foreach($multiplesThree as $multipleThree) {
+      $resultMultiplesThree[] = $fizzBuzz->print($multipleThree);
+    }
+
+    $this->assertEquals($resultExpected, $resultMultiplesThree);
+  }
 }
