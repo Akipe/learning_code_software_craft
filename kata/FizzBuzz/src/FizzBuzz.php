ddb7a479ad54283ce01ecfd18c280a387f0dc784
Julien Milletre Akipe <code.julien@milletre.fr>
2023-08-26 22:31:36 +0200

    Ajout de refactorisation de la vérification de la division

--- kata/FizzBuzz/src/FizzBuzz.php
+++ kata/FizzBuzz/src/FizzBuzz.php
@@ -9,18 +9,35 @@ class FizzBuzz
 
   public function print(int $number): string
   {
-    if ($number % 3 == 0 && $number % 5 == 0) {
+    if ($this->isMultipleOf($number, [3, 5])) {
       return self::FIZZ . self::BUZZ;
     }
 
-    if ($number % 3 == 0) {
+    if ($this->isMultipleOf($number, [3])) {
       return self::FIZZ;
     }
 
-    if ($number % 5 === 0) {
+    if ($this->isMultipleOf($number, [5])) {
       return self::BUZZ;
     }
 
     return (string) $number;
+  }
+
+  /**
+   * Check if $number is a multiple of several dividers.
+   *
+   * @param int $number to check if it can be divider
+   * @param int[] $dividers list of dividers to check if $number can be divider
+   * @return bool if the $number can be divide by all dividers.
+   */
+  private function isMultipleOf(int $number, array $dividers)
+  {
+    foreach($dividers as $divider) {
+      if ($number % $divider != 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
